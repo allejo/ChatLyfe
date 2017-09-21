5ec40be0e2cbe03490d56db2dd829640ba895da4
allejo <allejo@me.com>
2017-09-21 10:00:42 -0700

    Move deployment to external script

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# https://getcomposer.org/doc/faqs/how-to-install-composer-programmatically.md
+if [ ! -f "composer.phar" ]
+then
+    EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+
+    if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+    then
+        >&2 echo 'ERROR: Invalid installer signature'
+        rm composer-setup.php
+        exit 1
+    fi
+
+    php composer-setup.php --quiet
+    RESULT=$?
+    rm composer-setup.php
+fi
+
+# Get dependencies and handle actual deployment
+export SYMFONY_ENV=prod
+./composer.phar install --no-dev --optimize-autoloader
